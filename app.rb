206d167f377471049a48bed9e2c9e0354ff7e7f4
Jeremy Marin <jeremy.marin@minacolor.co.jp>
2023-06-26 22:35:26 +0900

    Add endpoint for each exercice. Remove old scripts

--- app.rb
+++ app.rb
@@ -1,5 +1,34 @@
-require 'sinatra'
+require "sinatra"
+require_relative "exercices/addition"
+require_relative "exercices/subtraction"
+require_relative "exercices/multiplication"
+require_relative "exercices/division"
+require_relative "document"
 
 get '/' do
   erb :index
+end
+
+get '/additions' do
+  content_type 'application/pdf'
+  questions = Document::TOTAL.times.map { Exercices::Addition::generate }
+  Document::build(title: "Additions", questions:).render
+end
+
+get '/soustractions' do
+  content_type 'application/pdf'
+  questions = Document::TOTAL.times.map { Exercices::Subtraction::generate }
+  Document::build(title: "Soustractions", questions:).render
+end
+
+get '/multiplications' do
+  content_type 'application/pdf'
+  questions = Document::TOTAL.times.map { Exercices::Multiplication::generate }
+  Document::build(title: "Multiplications", questions:).render
+end
+
+get '/divisions' do
+  content_type 'application/pdf'
+  questions = Document::TOTAL.times.map { Exercices::Division::generate }
+  Document::build(title: "Divisions", questions:).render
 end
--- generate_multiplications.rb
+++
@@ -1,6 +0,0 @@
-require_relative "exercices/multiplication"
-require_relative "document"
-
-questions = Document::TOTAL.times.map { Exercices::Multiplication::generate }
-
-Document::build(title: "Multiplications", questions:)
